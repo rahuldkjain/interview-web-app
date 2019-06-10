afb9aff580f2d83a43921c1fdc2d069b3d5ded34
Rahul Jain <rahuldkjain@gmail.com>
2019-06-10 19:40:25 +0530

    initial js

---
+++ js/interview.js
@@ -0,0 +1,3 @@
+document.getElementById("profile").style.display = 'none';
+document.getElementById("expertise").style.display = 'none';
+document.getElementById("interview").style.display = 'none';
